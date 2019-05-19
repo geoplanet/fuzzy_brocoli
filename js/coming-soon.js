3db537f450377b7b92412fb0a258f3b949962d8f
geoplanet <jeff@geoprism.ca>
2019-05-19 13:03:50 -0700

    Update coming-soon.js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,6 +1,10 @@
 (function($) {
+  function validateForm() {
+  var x = document.forms["mail_form"]["email"].value;
+  if (x == "") {
+    alert("Email field must be filled out");
+    return false;
+  }
+} 
   "use strict"; // Start of use strict
-
-  // No JS
-
 })(jQuery); // End of use strict
